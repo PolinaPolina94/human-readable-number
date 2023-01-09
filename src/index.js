5c40b5c335a3e6720b3cbcbca260f354ffba6a26
Полина <polina_kazhina@mail.ru>
2023-01-09 15:08:48 +0300

    feat: realize human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+
+    const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 
+                'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) return num[number];
+    const count = number%10;
+    if (number < 100) return count? tens[Math.floor((number/10)-2)] + " " + num[count] : tens[(number/10)-2];
+    if (number < 1000) return (number%100 === 0)? num[(number/100)] + " hundred" :  
+                          num[Math.floor(number/100)] + " hundred" + " " + toReadable(number%100);
 }
